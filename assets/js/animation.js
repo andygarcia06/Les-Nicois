12f306234e28b1d56c6cb86dde8820dfb8f20ad3
Andy Garcia <andygarcia@live.fr>
2024-07-25 19:30:06 +0200

    adding view transition

---
+++ assets/js/animation.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function() {
+    gsap.registerPlugin(ScrollTrigger);
+
+    const sections = ["#section-1", "#section-2", "#section-3"];
+
+    sections.forEach((id, index) => {
+        gsap.fromTo(id + " .content", 
+            { opacity: 0 }, 
+            { 
+                opacity: 1, 
+                duration: 3, // Durée de l'animation en secondes
+                scrollTrigger: {
+                    trigger: id,
+                    start: "top 80%",
+                    toggleActions: "play none none none"
+                }
+            }
+        );
+    });
+
+
+});
